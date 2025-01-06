459a8c25fc13f1ab98f14f1ae402a7ca3d89d95c
nm21205 <nm21205@naver.com>
2025-01-06 18:03:58 +0900

    250106

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,58 +1,62 @@
-import React from 'react';
+import React,{useState} from 'react';
+import data from '../data/data';
+import {Link} from 'react-router-dom';
+import { Navigation,Autoplay} from 'swiper/modules';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
 import './Intro.scss';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
-const Intro = () => {
+
+const Location = () => {
+  const [dataLocation] = useState(data)
   return (
-    <div>
-      <div className='intro-container'>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-            <img src={process.env.PUBLIC_URL + '/img/price1.png'} alt='intro1' />
-          </div>
+    <div className='location'>
+        <h2>Location</h2>
+        <p>신규 가맹점..</p>
+        <div className="slideLocation">
+          <Swiper
+            className='locationSwiper'
+            modules={[Navigation, Autoplay]}
+            spaceBetween={5}
+            slidesPerView={4}
+            navigation
+           loop={true}
+           autoplay={{
+            delay: 0,
+            disableOnInteraction: false,
+           }}
+           speed={5000}
+           loopAdditionalSlides={1}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+          >
+            {
+              dataLocation.map((data) => {
+                return (
+                  <SwiperSlide key={data.id}>
+                    <div className="locationList">
+                      <Link to="">
+                          <div className="imgBox">
+                            <img src={process.env.PUBLIC_URL+data.img} alt={data.title} />
+                          </div>
+                          <div className="textBox"><p>{data.title} </p></div>
+                          <div className="addBox"><p>{data.add} </p></div>
+                      </Link>
+                    </div>
+                  </SwiperSlide>
+                )
+              })
+            }
+           
+           
+          </Swiper>
         </div>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-            <img src={process.env.PUBLIC_URL + '/img/price2.png'} alt='intro1' />
-          </div>
-        </div>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-            <img src={process.env.PUBLIC_URL + '/img/price3.png'} alt='intro1' />
-          </div>
-        </div>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-            <img src={process.env.PUBLIC_URL + '/img/price4.png'} alt='intro1' />
-          </div>
-        </div>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-              <img src={process.env.PUBLIC_URL + '/img/price5.png'} alt='intro1' />
-          </div>
-        </div>
-        <div className='intro-title'>
-          <h1>특가 상품</h1>
-          <h2>300만원 이하</h2>
-          <div className='intro-img'>
-            <img src={process.env.PUBLIC_URL + '/img/price6.png'} alt='intro1' />
-          </div>
-        </div>
-        
-      </div>
     </div>
   );
 };
 
-export default Intro;
+export default Location;
